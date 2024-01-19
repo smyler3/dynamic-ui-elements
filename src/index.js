a7968f6822805445c95ba84638081501b9ab9d40
smyler3 <mylertalym01@gmail.com>
2024-01-19 19:51:33 +1100

    Add dropdown menu logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css";
+import { attachDropdownMenuButtonListener } from "./dropdownMenu";
+
+const dropdownButton = document.querySelector(".menu-visibility-button");
+const dropdownList = document.querySelector(".dropdown-menu");
+console.log(dropdownButton, dropdownList);
+attachDropdownMenuButtonListener(dropdownButton, dropdownList);
