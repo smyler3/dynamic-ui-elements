323cc156784196e0a2d4357dfb34ffd7f490b627
smyler3 <mylertalym01@gmail.com>
2024-01-19 20:16:50 +1100

    Add prettier and multiple dropdowns

--- src/dropdownMenu.js
+++ src/dropdownMenu.js
@@ -1,13 +1,13 @@
 // Changes the visibility of a given dropdown list
 function changeVisibility(dropdownMenu) {
-    dropdownMenu.classList.toggle("hidden");
+  dropdownMenu.classList.toggle("hidden");
 }
 
 // Allows a button to alter visibility of a dropdown list
 function attachDropdownMenuButtonListener(button, dropdownMenu) {
-    button.addEventListener("click", () => {
-        changeVisibility(dropdownMenu);
-    });
+  button.addEventListener("click", () => {
+    changeVisibility(dropdownMenu);
+  });
 }
 
-export { attachDropdownMenuButtonListener }
+export { attachDropdownMenuButtonListener };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import "./style.css";
 import { attachDropdownMenuButtonListener } from "./dropdownMenu";
 
-const dropdownButton = document.querySelector(".menu-visibility-button");
-const dropdownList = document.querySelector(".dropdown-menu");
-console.log(dropdownButton, dropdownList);
-attachDropdownMenuButtonListener(dropdownButton, dropdownList);
+const optionDropdownButton = document.querySelector(".option-button");
+const optionDropdownList = document.querySelector(".options-menu");
+attachDropdownMenuButtonListener(optionDropdownButton, optionDropdownList);
+
+const choiceDropdownButton = document.querySelector(".choice-button");
+const choideDropdownList = document.querySelector(".choices-menu");
+attachDropdownMenuButtonListener(choiceDropdownButton, choideDropdownList);
