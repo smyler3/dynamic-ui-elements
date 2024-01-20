a4bf31998a18450102d6dc02e1375476d1082519
smyler3 <mylertalym01@gmail.com>
2024-01-20 14:51:31 +1100

    Add initial carousel elements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import "./style.css";
 import { attachDropdownMenuButtonListener } from "./dropdownMenu";
 
-const openSidebarButton = document.querySelector(".open-sidebar-button");
-const sidebar = document.querySelector(".sidebar");
-attachDropdownMenuButtonListener(openSidebarButton, sidebar);
+// const openSidebarButton = document.querySelector(".open-sidebar-button");
+// const sidebar = document.querySelector(".sidebar");
+// attachDropdownMenuButtonListener(openSidebarButton, sidebar);
 
-const optionDropdownButton = document.querySelector(".option-button");
-const optionDropdownList = document.querySelector(".options-menu");
-attachDropdownMenuButtonListener(optionDropdownButton, optionDropdownList);
+// const optionDropdownButton = document.querySelector(".option-button");
+// const optionDropdownList = document.querySelector(".options-menu");
+// attachDropdownMenuButtonListener(optionDropdownButton, optionDropdownList);
 
-const choiceDropdownButton = document.querySelector(".choice-button");
-const choideDropdownList = document.querySelector(".choices-menu");
-attachDropdownMenuButtonListener(choiceDropdownButton, choideDropdownList);
+// const choiceDropdownButton = document.querySelector(".choice-button");
+// const choideDropdownList = document.querySelector(".choices-menu");
+// attachDropdownMenuButtonListener(choiceDropdownButton, choideDropdownList);
